65570c37c574e465ecbd80ee1d31293b84102318
Luiz Fernando Rezende <67608240+luizfrezende@users.noreply.github.com>
2021-11-03 19:58:18 -0300

    Add files via upload

---
+++ Global_COVID19_Tracker.js
@@ -0,0 +1,6 @@
+﻿function initViz() {
+    var containerDiv = document.getElementById("vizContainer"),
+        url = "https://public.tableau.com/views/COVID-19Cases_15840488375320/COVID-19GlobalView?:language=pt-BR&:display_count=n&:origin=viz_share_link";
+
+    var viz = new tableau.Viz(containerDiv, url);
+}
